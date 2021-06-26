5505052c8154a7e85e626698ea28cf04289d2ddc
koliadjun <koliadjun@gmail.com>
2021-06-26 19:42:32 +0300

    implement search engine

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
-import './sass/main.scss';
-console.log('HI');
+import fetchCountries from "./js/fetchCountries.js";
+import countryCardTmp from './templates/countryTpl.hbs'
+const debounce = require('lodash.debounce');
+
+const refs = {
+    countryCard: document.querySelector('.country-card'),
+    input: document.querySelector('#search'),
+}
+
+const onSearch = (event) => {
+    let value = event.target.value;
+    if (!value) { return }
+    fetchCountries(event.target.value).then(render);
+}
+const renderCard = (data) => {
+    refs.countryCard.innerHTML = '';
+    refs.countryCard.insertAdjacentHTML("beforeend", countryCardTmp(data))
+}
+const render = (data) => {
+    const countryQuantity = data.length;
+    if (countryQuantity) {
+        if (countryQuantity === 1) {
+            renderCard(data[0]);
+        }
+        if (countryQuantity > 1 && countryQuantity <= 10) {
+            console.log('result 1 and 10', + countryQuantity);
+        }
+        if (countryQuantity > 10) {
+            console.log(`more than 10 ${countryQuantity}`)
+        }
+    } else {
+        console.log('result 0');
+    }
+
+
+}
+refs.input.addEventListener('input', debounce(onSearch, 500));
